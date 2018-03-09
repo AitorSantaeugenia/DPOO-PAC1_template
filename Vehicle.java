d664b0ec7608f287947882e09fc1b71054c5668e
Aitor J. Santaeugenia <aitor.j.s.m@gmail.com>
2018-03-09 12:12:30 +0100

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,42 @@
+
+public class Vehicle {
+	
+	private String nameVehicle;
+	int priceVehicle;
+	int numberSeats;
+	private String typeVehicle;
+	
+	public String getNameVehicles() {
+		return nameVehicle;
+		
+	}
+	
+	public void setNameVehicles(String nameVehicle) {
+		this.nameVehicle = nameVehicle;
+	}
+	
+	public int getPriceVehicles() {
+		return priceVehicle;
+		
+	}
+	
+	public void setPriceVehicles(int priceVehicle) {
+		this.priceVehicle = priceVehicle;
+	}
+	
+	public int getNumberSeats() {
+		return numberSeats;
+	}
+	
+	public void setNumberSeats(int numberSeats) {
+		this.numberSeats = numberSeats;
+	}
+	
+	public String getTypeVehicles() {
+		return typeVehicle;
+	}
+	
+	public void setTypeVehicles(String typeVehicle) {
+		this.typeVehicle = typeVehicle;
+	}
+}
