bd3de37bbd12abf7d3ebc9b7e13980cf6ceb1ff3
Aitor J. Santaeugenia <aitor.j.s.m@gmail.com>
2018-03-09 12:09:57 +0100

    Add files via upload

---
+++ Engine.java
@@ -0,0 +1,13 @@
+
+public class Engine {
+	private String typeEngine;
+
+	public String getTypeEngine() {
+		return typeEngine;
+		
+	}
+	
+	public void setTypeEngine(String typeEngine) {
+		this.typeEngine = typeEngine;
+	}
+}
