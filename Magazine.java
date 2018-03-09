c5e006fb6af27b8710198b22fee14042a6e263e4
Aitor J. Santaeugenia <aitor.j.s.m@gmail.com>
2018-03-09 12:12:08 +0100

    Add files via upload

---
+++ Magazine.java
@@ -0,0 +1,31 @@
+
+public class Magazine {
+	int priceMagazine;
+	private String nameMagazine;
+	int numberMagazine;
+	
+	public int getPriceMagazine(){
+		return priceMagazine;
+	}
+	
+	public void setPriceMagazine(int priceMagazine){
+		this.priceMagazine = priceMagazine;
+	}
+	
+	public String getNameMagazine() {
+		return nameMagazine;
+	}
+	
+	public void setNameMagazine(String nameMagazine) {
+		this.nameMagazine = nameMagazine;
+	}
+	
+	public int getNumberMagazine() {
+		return numberMagazine;
+	}
+	
+	public void setNumberMagazine(int numberMagazine) {
+		this.numberMagazine = numberMagazine;
+	}
+		
+}
