4e843d892b46b609f17e9bb0a1977886e8faaab0
Aitor J. Santaeugenia <aitor.j.s.m@gmail.com>
2018-03-09 12:12:19 +0100

    Add files via upload

---
+++ Score.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+
+public class Score {
+	public Date dateScore;
+	int numberScore;
+
+	public Date getDateScore() {
+		return dateScore;
+		
+	}
+	
+	public void setDateScore(Date dateScore) {
+		this.dateScore = dateScore;
+	}
+	
+	public int getNumberScore() {
+		return numberScore;
+	}
+	
+	public void setNumberScore(int numberScore) {
+		this.numberScore = numberScore;
+	}
+}
